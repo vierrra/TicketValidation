a14cd439b59bbf56882941fd4c225682d587ceef
Renato <renato.vieira@stant.com.br>
2021-06-24 16:49:56 -0300

    Initial Commit

---
+++ TicketsValidation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicketsValidation
+//
+//  Created by Renato on 6/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
