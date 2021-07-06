30c8a1fb06890206ed26747a1561aee124d240f2
Renato <renato.vieira@stant.com.br>
2021-07-06 19:15:56 -0300

    create layout

--- TicketsValidation/ViewController.swift
+++ TicketsValidation/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bannerImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.configureBannerImageView()
+       
     }
 
-
+    func configureBannerImageView() {
+        self.bannerImageView.layer.cornerRadius  = 10
+        self.bannerImageView.layer.masksToBounds = true
+    }
 }
 
